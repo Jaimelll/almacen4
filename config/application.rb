62963f5596b251d1a52eab2f75525720bdb4b563
Jaimelll <jaduran0102@gmail.com>
2021-02-02 11:41:48 -0500

    configuracion

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Almacen4
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "America/Lima"
     # config.eager_load_paths << Rails.root.join("extras")
     config.assets.initialize_on_precompile = false
   end
